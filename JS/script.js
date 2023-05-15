fe9d692e17fea25f5d526cf83508be8d4b8555c8
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-15 16:21:36 +0200

    Bonus soluction

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
-const kilometri = document.getElementById("mykm").value;
+const kilometri = parseInt(document.getElementById("mykm").value);
 
 console.log(kilometri);
 
-const myage = document.getElementById("myage").value;
+const myage =  parseInt(document.getElementById("myage").value);
 
 console.log(myage);
 
@@ -11,17 +11,34 @@ let ticketprice = 0.233 * kilometri;
 const button = document.querySelector("button.mybutton")
 
 button.addEventListener("click", function(){
+
+    if (isNaN(kilometri) && isNaN(myage)) {
+        document.getElementById("error").innerHTML = "Hai inserito un valore non valido per i kilometri e per l'età!";
+    
+        alert("Hai inserito un valore non valido per i kilometri e per l'età!");
+    
+      } else if (isNaN(kilometri)) {
+        document.getElementById("error").innerHTML = "Hai inserito un valore non valido per i kilometri!";
+    
+        alert("Hai inserito un valore non valido per i kilometri!");
+    
+      } else if (isNaN(myage)) {
+        document.getElementById("error").innerHTML = "Hai inserito un valore non valido per l'età!";
+    
+        alert("Hai inserito un valore non valido per l'età!");
+        
+      } else {
+      
+        if (myage < 18) {
+            ticketprice = ticketprice - ((ticketprice * 19.4) / 100);
+        } else if (myage > 65) {
+            ticketprice = ticketprice - ((ticketprice * 37.7) / 100);
+        }
+    
+    }
     document.getElementById("answer").innerHTML += "IL PREZZO E': " + ticketprice.toFixed(2) + " €" ;
 
     console.log(ticketprice.toFixed(2));
 })
 
-if (myage < 18){
 
-    ticketprice = ticketprice - ((ticketprice * 19.4) / 100);
-
-} else if (myage > 65){
-
-    ticketprice = ticketprice - ((ticketprice * 37.7) / 100);
-    
-}
