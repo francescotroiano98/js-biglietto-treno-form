f90058d0778e450c507e9707472a82e2b7c6aa88
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-15 16:01:28 +0200

    Done Milestone 2

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,27 @@
 const kilometri = document.getElementById("mykm").value;
+
 console.log(kilometri);
+
 const myage = document.getElementById("myage").value;
+
 console.log(myage);
-const button = document.querySelector("button.mybutton")
-button.addEventListener("click", function(){
-    console.log(ticketprice);
-})
+
 let ticketprice = 0.233 * kilometri;
 
+const button = document.querySelector("button.mybutton")
+
+button.addEventListener("click", function(){
+    document.getElementById("answer").innerHTML += "IL PREZZO E': " + ticketprice.toFixed(2) + " €" ;
+
+    console.log(ticketprice.toFixed(2));
+})
 
 if (myage < 18){
+
     ticketprice = ticketprice - ((ticketprice * 19.4) / 100);
+
 } else if (myage > 65){
+
     ticketprice = ticketprice - ((ticketprice * 37.7) / 100);
+    
 }
