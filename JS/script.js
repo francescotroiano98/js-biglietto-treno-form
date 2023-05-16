408bb3e4e235393cb520a93b0565c1e3199da7f6
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-16 11:12:52 +0200

    fix soluction

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,12 @@
-const button = document.querySelector("button.mybutton")
+
+
+const button = document.querySelector("button.mybutton");
+
+const buttonreset = document.querySelector("button.reset");
+
+buttonreset.addEventListener("click", function(){
+document.getElementById("answer").innerHTML = "Rinserisci i valori";
+})
 
 button.addEventListener("click", function(){
     const kilometri = parseInt(document.getElementById("mykm").value);
@@ -9,18 +17,21 @@ button.addEventListener("click", function(){
     
         alert("Hai inserito un valore non valido per i kilometri e per l'età!");
     
+      } else if (kilometri <= 0) {
+        document.getElementById("error").innerHTML = "Il numero di kilometri deve essere superiore a zero!";
+        alert("Il numero di kilometri deve essere superiore a zero!");
+        
       } else if (isNaN(kilometri)) {
         document.getElementById("error").innerHTML = "Hai inserito un valore non valido per i kilometri!";
     
         alert("Hai inserito un valore non valido per i kilometri!");
     
-      } else if (isNaN(myage)) {
-        document.getElementById("error").innerHTML = "Hai inserito un valore non valido per l'età!";
-    
-        alert("Hai inserito un valore non valido per l'età!");
+      } else if (myage < 0 || myage > 120) {
+        document.getElementById("error").innerHTML = "L'età deve essere compresa tra 0 e 120!";
+        alert("L'età deve essere compresa tra 0 e 120!");
         
       } else {
-       let ticketprice = 0.233 * kilometri;;
+       let ticketprice = 0.233 * kilometri;
       
         if (myage < 18) {
             ticketprice = ticketprice - ((ticketprice * 19.4) / 100);
@@ -28,7 +39,7 @@ button.addEventListener("click", function(){
             ticketprice = ticketprice - ((ticketprice * 37.7) / 100);
         }
     
-    document.getElementById("answer").innerHTML += "IL PREZZO E': " + ticketprice.toFixed(2) + " €" ;
+    document.getElementById("answer").innerHTML = "IL PREZZO E': " + ticketprice.toFixed(2) + " €" ;
     
   }
 })
